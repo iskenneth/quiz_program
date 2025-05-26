f443b2e0ca8d9184c77df2a02cf81fd097f3b029
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-05-26 11:24:02 +0800

    save file to txt

--- quiz_creator.py
+++ quiz_creator.py
@@ -7,4 +7,12 @@ from kivy.uix.button import Button
 from kivy.uix.spinner import Spinner
 
 #convert input to txt 
+def convert_to_txt (question, choice_a, choice_b,choice_c, choice_d, correct_answer): 
+    with open('quiz_data.txt' , 'a') as file:
+        file.write(f" Question: {question} \n")
+        file.write(f" a.): {choice_a} \n")
+        file.write(f" b.): {choice_b} \n")
+        file.write(f" c.): {choice_c} \n")
+        file.write(f" d.): {choice_d} \n")
+        file.write(f" Correct Answer: {correct_answer} \n")
 #ask input questiom andnchoices and correct answer
