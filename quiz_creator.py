485bf521e577a5d9636ba0428f1e9057ace24c7f
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-05-26 11:16:47 +0800

    import libraries

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,3 +1,10 @@
 #import libraries
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button 
+from kivy.uix.spinner import Spinner
+
 #convert input to txt 
 #ask input questiom andnchoices and correct answer
