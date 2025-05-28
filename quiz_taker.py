594653f600120ad4ad5fe5dee9fa7f1b6061d9c6
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.phcd /storage/emulated/0/pydroid3/ OOP PROJECT COMPILATIONgit initgit add .git commit -m Initial>
2025-05-28 18:37:44 +0800

    variables for question choices and correct answer

--- quiz_taker.py
+++ quiz_taker.py
@@ -17,4 +17,11 @@ def load_question_from_file(filename):
     with open ('quiz_data.txt', 'r') as file:
         lines = file.readlines()      
     for options in range (0,(len), 6):
+        question_text = lines[options].strip().split(":")[1]. strip()
+        choice_a = lines[options + 1].strip().split(":")[1]. strip()
+        choice_b = lines[options + 2].strip().split(":")[1]. strip()
+        choice_c = lines[options + 3].strip().split(":")[1]. strip()
+        choice_d=lines[options + 4].strip().split(":")[1], strip()
+        correct_answer = lines[options + 5].strip(). split(":")[1].strip()
+        
                   
